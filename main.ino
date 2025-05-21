3ccbfac84b5585f8f1d26217acb45875b3535ed7
Svetlin <93772733+Svetlin56@users.noreply.github.com>
2025-05-21 18:35:50 +0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,113 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include "arducam_dvp.h"
+#include "Arduino_H7_Video.h"
+#include "dsi.h"
+#include "SDRAM.h"
+#include <PubSubClient.h>
+
+#define ARDUCAM_CAMERA_OV767X
+#define IMAGE_MODE CAMERA_RGB565
+
+#ifdef ARDUCAM_CAMERA_HM01B0
+#include "Himax_HM01B0/himax.h"
+HM01B0 himax;
+Camera cam(himax);
+#elif defined(ARDUCAM_CAMERA_OV767X)
+#include "OV7670/ov767x.h"
+OV7675 ov767x;
+Camera cam(ov767x);
+#endif
+
+FrameBuffer fb;
+FrameBuffer outfb;
+Arduino_H7_Video Display(800, 480, GigaDisplayShield);
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+#define SOIL_PIN A0
+#define SOLAR_PIN A1
+#define LDR_PIN A2
+#define REFERENCE_VOLTAGE 5
+#define VOLTAGE_DIVIDER_RATIO 2.0
+
+DHT dht(DHTPIN, DHTTYPE);
+float temperature = 0.0, humidity = 0.0, solarVoltage = 0.0;
+int soilMoisture = 0, lightLevel = 0;
+
+#define HTONS(x) (((x >> 8) & 0x00FF) | ((x << 8) & 0xFF00))
+
+void blinkLED(uint32_t count = 0xFFFFFFFF) {
+  pinMode(LED_BUILTIN, OUTPUT);
+  while (count--) {
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(50);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(50);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  pinMode(SOIL_PIN, INPUT);
+  pinMode(SOLAR_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
+
+  delay(3000);
+  if (!cam.begin(CAMERA_R320x240, IMAGE_MODE, 30)) {
+    blinkLED();
+  }
+
+  Display.begin();
+  outfb.setBuffer((uint8_t*)SDRAM.malloc(1024 * 1024));
+  dsi_lcdClear(0);
+  dsi_drawCurrentFrameBuffer();
+  dsi_lcdClear(0);
+  dsi_drawCurrentFrameBuffer();
+
+  Serial.println("System start: camera + sensors.");
+}
+
+void readSensors() {
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  soilMoisture = analogRead(SOIL_PIN);
+  int raw = analogRead(SOLAR_PIN);
+  solarVoltage = (raw / 1023.0) * REFERENCE_VOLTAGE * VOLTAGE_DIVIDER_RATIO;
+  lightLevel = analogRead(LDR_PIN);
+}
+
+void logSensorData() {
+  Serial.print("DATA:");
+  Serial.print(temperature); Serial.print(",");
+  Serial.print(humidity); Serial.print(",");
+  Serial.print(soilMoisture); Serial.print(",");
+  Serial.print(lightLevel); Serial.print(",");
+  Serial.println(solarVoltage, 2);
+}
+
+
+void loop() {
+  readSensors();
+  logSensorData();
+
+  if (cam.grabFrame(fb, 3000) == 0) {
+    for (int i = 0; i < 320; i++) {
+      for (int j = 0; j < 240; j++) {
+        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480 + 1] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480 + 1] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+      }
+    }
+
+    dsi_lcdDrawImage((void*)outfb.getBuffer(), (void*)dsi_getCurrentFrameBuffer(), 480, 640, DMA2D_INPUT_RGB565);
+    dsi_drawCurrentFrameBuffer();
+  } else {
+    Serial.println("Can't process image.");
+    blinkLED(3);
+  }
+
+  delay(10000);
+}
