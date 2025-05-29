495850c6b9e168ed2c729a2a4484ac5d24712658
Svetlin <93772733+Svetlin56@users.noreply.github.com>
2025-05-29 13:57:59 +0300

    Update main.ino
    added mongo support

--- main.ino
+++ main.ino
@@ -6,17 +6,32 @@
 #include "SDRAM.h"
 #include <PubSubClient.h>
 
-#define ARDUCAM_CAMERA_OV767X
-#define IMAGE_MODE CAMERA_RGB565
+#define ARDUCAM_CAMERA_HM0360
 
 #ifdef ARDUCAM_CAMERA_HM01B0
 #include "Himax_HM01B0/himax.h"
 HM01B0 himax;
 Camera cam(himax);
+
+#define IMAGE_MODE CAMERA_GRAYSCALE
+#elif defined(ARDUCAM_CAMERA_HM0360)
+#include "Himax_HM0360/hm0360.h"
+HM0360 himax;
+Camera cam(himax);
+
+#define IMAGE_MODE CAMERA_GRAYSCALE
 #elif defined(ARDUCAM_CAMERA_OV767X)
 #include "OV7670/ov767x.h"
 OV7675 ov767x;
 Camera cam(ov767x);
+
+#define IMAGE_MODE CAMERA_RGB565
+#elif defined(ARDUCAM_CAMERA_GC2145)
+#include "GC2145/gc2145.h"
+GC2145 galaxyCore;
+Camera cam(galaxyCore);
+
+#define IMAGE_MODE CAMERA_RGB565
 #endif
 
 FrameBuffer fb;
@@ -28,16 +43,15 @@ Arduino_H7_Video Display(800, 480, GigaDisplayShield);
 #define SOIL_PIN A0
 #define SOLAR_PIN A1
 #define LDR_PIN A2
-#define REFERENCE_VOLTAGE 5
+#define REFERENCE_VOLTAGE 3.3
 #define VOLTAGE_DIVIDER_RATIO 2.0
 
 DHT dht(DHTPIN, DHTTYPE);
 float temperature = 0.0, humidity = 0.0, solarVoltage = 0.0;
 int soilMoisture = 0, lightLevel = 0;
 
-#define HTONS(x) (((x >> 8) & 0x00FF) | ((x << 8) & 0xFF00))
-
-void blinkLED(uint32_t count = 0xFFFFFFFF) {
+void blinkLED(uint32_t count = 0xFFFFFFFF)
+{
   pinMode(LED_BUILTIN, OUTPUT);
   while (count--) {
     digitalWrite(LED_BUILTIN, LOW);
@@ -47,27 +61,39 @@ void blinkLED(uint32_t count = 0xFFFFFFFF) {
   }
 }
 
+uint32_t palette[256];
+
+
 void setup() {
   Serial.begin(115200);
   dht.begin();
   pinMode(SOIL_PIN, INPUT);
   pinMode(SOLAR_PIN, INPUT);
   pinMode(LDR_PIN, INPUT);
-
   delay(3000);
+
   if (!cam.begin(CAMERA_R320x240, IMAGE_MODE, 30)) {
     blinkLED();
   }
 
+  for (int i = 0; i < 256; i++) {
+    palette[i] = 0xFF000000 | (i << 16) | (i << 8) | i;
+  }
+
   Display.begin();
+
+  if (IMAGE_MODE == CAMERA_GRAYSCALE) {
+    dsi_configueCLUT((uint32_t*)palette);
+  }
   outfb.setBuffer((uint8_t*)SDRAM.malloc(1024 * 1024));
   dsi_lcdClear(0);
   dsi_drawCurrentFrameBuffer();
   dsi_lcdClear(0);
   dsi_drawCurrentFrameBuffer();
-
-  Serial.println("System start: camera + sensors.");
 }
+
+#define HTONS(x)    (((x >> 8) & 0x00FF) | ((x << 8) & 0xFF00))
+
 
 void readSensors() {
   temperature = dht.readTemperature();
@@ -93,20 +119,26 @@ void loop() {
   logSensorData();
 
   if (cam.grabFrame(fb, 3000) == 0) {
+
     for (int i = 0; i < 320; i++) {
       for (int j = 0; j < 240; j++) {
-        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
-        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480 + 1] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
-        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
-        ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480 + 1] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+        if (IMAGE_MODE == CAMERA_GRAYSCALE) {
+          ((uint8_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480] = ((uint8_t*)fb.getBuffer())[i + j * 320];
+          ((uint8_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480 + 1] = ((uint8_t*)fb.getBuffer())[i + j * 320];
+          ((uint8_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480] = ((uint8_t*)fb.getBuffer())[i + j * 320];
+          ((uint8_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480 + 1] = ((uint8_t*)fb.getBuffer())[i + j * 320];
+        } else {
+          ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+          ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2) * 480 + 1] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+          ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+          ((uint16_t*)outfb.getBuffer())[j * 2 + (i * 2 + 1) * 480 + 1] = HTONS(((uint16_t*)fb.getBuffer())[i + j * 320]);
+        }
       }
     }
-
-    dsi_lcdDrawImage((void*)outfb.getBuffer(), (void*)dsi_getCurrentFrameBuffer(), 480, 640, DMA2D_INPUT_RGB565);
+    dsi_lcdDrawImage((void*)outfb.getBuffer(), (void*)dsi_getCurrentFrameBuffer(), 480, 640, IMAGE_MODE == CAMERA_GRAYSCALE ? DMA2D_INPUT_L8 : DMA2D_INPUT_RGB565);
     dsi_drawCurrentFrameBuffer();
   } else {
-    Serial.println("Can't process image.");
-    blinkLED(3);
+    blinkLED(20);
   }
 
   delay(10000);
